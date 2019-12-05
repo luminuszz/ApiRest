b948b30d940b138f6d536a289525430470d29414
davi ribeiro <luminuszz43@gmail.com>
2019-12-05 09:37:29 -0300

    validando token jwt na aplicação

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 const authConfig = require('../../config/auth');
-
-module.exports = (req, res, next)=>{
+const {promisify} = require('util');
+module.exports = async (req, res, next)=>{
   const authHeader = req.headers.authorization;
 
 
@@ -12,8 +12,11 @@ module.exports = (req, res, next)=>{
   const [, token] = authHeader.split(' ');
 
   try {
-    jwt.verify(token, authConfig.secret);
-  } catch (err) {
-
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+    req.userId = decoded.id;
+    return next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({error: 'token invalid'});
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,20 @@
 const express = require('express');
 
-
 // eslint-disable-next-line new-cap
 const routes = express.Router();
 
 const UserController = require('./app/controllers/UserController');
 const SessionController = require('./app/controllers/SessionController');
 
+const authMiddleware = require('./app/middlewares/auth');
+
+// Middlewares Routes
+routes.get('/teste', authMiddleware, (req, res) =>
+  res.json({ok: true}),
+);
+
+// Controllers Routes
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
-
 
 module.exports = routes;
