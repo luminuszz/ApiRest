b8c7f5ab4d1f2f04ec98cf565fd4377396d5c88d
davi ribeiro <luminuszz43@gmail.com>
2019-12-06 13:42:49 -0300

    Itegrando Sentry para logs de erro em ambiente de produção

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const Sentry = require('@sentry/node');
+const sentryConfig = require('./config/sentry');
 const mongoose = require('mongoose');
 const validate = require('express-validation');
 const Youch = require('youch');
@@ -7,11 +9,17 @@ class App {
   constructor() {
     this.express = express();
     this.isDev = process.env.Node_ENV !== 'production';
+    this.sentry();
     this.database();
     this.middleswares();
     this.routes();
     this.exceptions();
   }
+  sentry( ) {
+    Sentry.init(sentryConfig);
+  }
+
+
   database() {
     mongoose.connect('mongodb://localhost:27017/bankapi', {
       useNewUrlParser: true,
@@ -22,21 +30,27 @@ class App {
 
   middleswares() {
     this.express.use(express.json());
+    this.express.use(Sentry.Handlers.requestHandler());
   }
   routes() {
     this.express.use(require('./routes'));
   }
 
   exceptions() {
+    /**
+    *  if (process.env.Node_ENV === 'production') {
+    * }
+    */
+    this.express.use(Sentry.Handlers.errorHandler());
     this.express.use(async (err, req, res, next) => {
       if (err instanceof validate.ValidationError) {
         return res.status(err.status).json(err);
       }
 
       if (process.env.Node_ENV !== 'production') {
-        const youch = new Youch(err);
+        const youch = new Youch(err, req);
 
-        return res.json( await youch.toJSON());
+        return res.send( await youch.toHTML());
       }
       {return res.status(err.status || 500).json({error: 'Internal Error'});}
     });
